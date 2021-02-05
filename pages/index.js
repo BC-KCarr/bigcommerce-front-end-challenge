cfbe4f299637edcaedd388d1cd64e605dc196ab6
Kevin <kevincarr632@gmail.com>
2021-02-05 15:47:24 -0600

    works on refresh. problem with use of getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,64 @@
-import Head from 'next/head'
+import React, { useState } from 'react'
+import Pagination from '@material-ui/lab/Pagination'
 import styles from '../styles/Home.module.css'
+import { withRouter } from 'next/router'
 
-export default function Home() {
+function Home({ data, totalPages, router }) {
+  const [page, setPage] = useState(1)
+
+  const handleChange = (event, value) => {
+    setPage(value)
+    const currentPath = router.pathname;
+    const currentQuery = router.query;
+    currentQuery.index = value - 1
+
+    router.push({
+      pathname: currentPath,
+      query: currentQuery,
+    });
+  }
+
   return (
     <div className={styles.container}>
-      
+  
+      <main className={styles.main}>
+        <div className={styles.headerContainer}>
+          <Pagination
+            count={totalPages}
+            page={page}
+            onChange={handleChange}
+          />
+          <h1 className={styles.title}>{data.title}</h1>
+        </div>
+        <img src={data.image.url} alt='' />
+      </main>
+
+      <footer className={styles.footer}>
+        <div>all right reserved</div>
+      </footer>
     </div>
   )
 }
+
+export const getServerSideProps = async (props) => {
+
+  const entryIndex = parseInt(props.query.index) || 0
+  async function fetchData(url) {
+    const response = await fetch(url)
+    return response.json();
+  }
+  return fetchData('https://www.bigcommerce.com/actions/bcCore/interview/getShowcaseEntryIds')
+    .then(ids => {
+      return fetchData(`https://www.bigcommerce.com/actions/bcCore/interview/getShowcaseEntryById?id=${ids[entryIndex]}`)
+        .then(response => {
+          return {
+            props: {
+              data: response,
+              totalPages: ids.length
+            }
+          }
+        })
+    })
+}
+
+export default withRouter(Home)
